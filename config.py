6426f909b235a6558a23a46db49565aee5710fc9
Athameral <athameral@outlook.com>
2024-12-07 15:34:05 +0800

    Modified config

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 from dataclasses import dataclass
+from typing import TypedDict
 import re
+import json
 
+class SMTPConfig(TypedDict):
+    server: str
+    port: int
+    username: str
+    password: str
 
 @dataclass
 class Config:
     username: str
     password: str
     email_list: list[str]
-    smtp_username: str
-    smtp_password: str
+    smtp: SMTPConfig
     check_interval_normal: float
     check_interval_urgent: float
 
@@ -17,8 +23,11 @@ class Config:
             if not self._validate_email(email):
                 raise ValueError(f"Invalid email address: {email}")
 
-
     @staticmethod
     def _validate_email(email: str) -> bool:
         pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         return re.match(pattern, email) is not None
+
+
+with open("./cfg.json", encoding="UTF-8") as f:
+    config = Config(**json.load(f))
--- emailhelper.py
+++ emailhelper.py
@@ -1,14 +1,14 @@
 from smtplib import SMTP_SSL
 from email.message import EmailMessage
 from time import localtime
-
+from config import config
 
 def send_remaining(From: str, To: str, remaining: float, password: str):
-    with SMTP_SSL("smtp.tongji.edu.cn") as server:
+    with SMTP_SSL(config.smtp["server"], config.smtp["port"]) as server:
         msg = EmailMessage()
         msg["From"] = From
         msg["To"] = To
         time = localtime()
-        msg["Subject"] = f"{time.tm_year}-{time.tm_mon}-{time.tm_mday} {time.tm_hour}时，剩余{remaining}度"
+        msg["Subject"] = f"{time.tm_year}-{time.tm_mon}-{time.tm_mday} {time.tm_hour}:{time.tm_min}:{time.tm_sec}，剩余{remaining}度"
         server.login(From, password)
         server.send_message(msg)
